d9e2c1cab77f47f3f233e05067fc95929ff2e4e7
Badlose <git@github.com:Badlose/helloworld.git>
2025-04-28 23:49:00 +0300

    Sketched auth service

--- src/main/java/ru/skypro/homework/dto/give/Comments.java
+++ src/main/java/ru/skypro/homework/dto/give/Comments.java
@@ -1,17 +1,20 @@
 package ru.skypro.homework.dto.give;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @Schema(description = "ДТО для предоставления информации обо всех комментариях")
+@AllArgsConstructor
 public class Comments {
 
     @NotBlank(message = "Count of Comments is required")
     private Integer count;
-    @NotBlank(message = "Comments are required")
+    @NotNull
     private List<Comment> results;
 }
