aa73eafe0d6ed180290fc1bcf64c55de46edd401
Badlose <git@github.com:Badlose/helloworld.git>
2025-05-04 00:27:39 +0300

    Sketched auth service. Some progress

--- src/main/java/ru/skypro/homework/entity/UserEntity.java
+++ src/main/java/ru/skypro/homework/entity/UserEntity.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.skypro.homework.dto.Role;
 import uk.co.jemos.podam.common.PodamExclude;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Table(name = "users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserEntity {
 
